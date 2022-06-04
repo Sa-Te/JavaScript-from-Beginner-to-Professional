e5c0c303828c57e245353764811b4e9fe0c992e1
Sa-Te <106857731+Sa-Te@users.noreply.github.com>
2022-06-04 10:34:49 +0530

    Update Exercise_5.3.js

--- Chapter 05/Exercise_5.3.js
+++ Chapter 05/Exercise_5.3.js
@@ -2,7 +2,7 @@ const myWork = [];
 for (let x = 1; x < 10; x++) {
     let stat = x % 2 ? true : false;
     let temp = {
-        name: `Lesson ${x}`, status: stat
+        name: `Lesson ${x}`, status: ${stat}
     };
     myWork.push(temp);
 }
